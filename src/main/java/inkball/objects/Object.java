f298f707e2f902764b5ca97ac2cdede89fd09827
ellacay <159973472+ellasmithssw@users.noreply.github.com>
2024-10-24 22:13:25 +1100

    config

--- src/main/java/inkball/objects/Object.java
+++ src/main/java/inkball/objects/Object.java
@@ -37,6 +37,10 @@ public abstract class Object {
         position.y = newY;
     }
 
+    public void setPosition(PVector newPosition) {
+        position = newPosition;
+    }
+
     public void setX(float newX) {
         position.x = newX;
     }
