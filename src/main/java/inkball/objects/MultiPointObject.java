a31f11a6113c36f80018408fdd98f42587e35ebd
ellacay <159973472+ellasmithssw@users.noreply.github.com>
2024-10-25 13:18:44 +1100

    Revert "extension enabled"
    This reverts commit a54ad531c18384300e8d044b9471e103f2203f0a.

---
+++ src/main/java/inkball/objects/MultiPointObject.java
@@ -0,0 +1,14 @@
+package inkball.objects;
+
+public class MultiPointObject {
+    public float x1, x2, y1, y2;
+
+    public MultiPointObject(float x1, float y1, float x2, float y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    
+}
